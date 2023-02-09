3eba54a1df42bc6811e740925ff17349989fabc8
Wojciech Grabowski (PGS Software) <wgrabowski@pgs-soft.com>
2023-02-09 22:25:10 +0100

    Make e2e work and add it to main Jenkinsfile

--- e2e/src/e2e/app.cy.ts
+++ e2e/src/e2e/app.cy.ts
@@ -1,13 +1,10 @@
-import { getGreeting } from '../support/app.po';
+import { getCalculator } from '../support/app.po';
 
 describe('pipelines-demo', () => {
   beforeEach(() => cy.visit('/'));
 
-  it('should display welcome message', () => {
-    // Custom command example, see `../support/commands.ts` file
-    cy.login('my-email@something.com', 'myPassword');
+  it('should display app name ', () => {
 
-    // Function helper example, see `../support/app.po.ts` file
-    getGreeting().contains('Welcome pipelines-demo');
+    getCalculator().contains('Giga Sumator');
   });
 });
